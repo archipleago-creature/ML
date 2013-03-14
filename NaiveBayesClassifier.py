3d14d9913333bfde0bcfee4b84a0c72b0d6e5a87
Justin Pati <justin@math.uu.se>
2013-03-14 10:06:40 +0100

    added log liklihood functionality to naive bayes to avoid numerical underflow.

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -27,30 +27,45 @@ class NaiveBayesClassifier:
         ###get feature priors
         for f in self.features:
             for k in self.classes:       
-                if not self.feature_probs.has_key((f,k)):
+                if 1:#not self.feature_probs.has_key((f,k)):
                     #print 'setting feature prob', f, k ,'.'
-                    self.feature_probs.setdefault((f,k),[]).append(self.likelihood_feature_given_class(f,k)) 
+                    temp=self.likelihood_feature_given_class(f,k)
+                    if not temp==dc.Decimal(0.0):
+                        self.feature_probs.setdefault((f,k),[]).append(temp) 
+        print 'for debugging we show everything we got under the training time.'
+        #print 'features',self.features
+        print 'classes', self.classes
+        print 'class probabilities', self.class_prob
+        #print 'feature_probs', self.feature_probs
+        print 'class counts', self.class_counts 
         #print self.feature_probs
         self.train=True
         return None
+
     def classify(self, feature_vector):
-        max1=0.0
+        max1=self.log_lik_example_cond_class(feature_vector,self.classes[0]) +dc.Decimal(self.class_prob[0]).ln()
         max_class=self.classes[0]
         for y in self.classes:
-            temp=self.max_lik_class_given_example(feature_vector, y)
-            #print 'a check in classify method', feature_vector.sparse_vec, temp
+            #    
+            #temp=self.max_lik_class_given_example(feature_vector, y)
+            temp=self.log_lik_example_cond_class(feature_vector, y) +dc.Decimal(self.class_prob[y]).ln()
             if temp > max1:
                 max1=temp
                 max_class=y
-        return (max_class, max1)
-    
+        return (max_class, max1.exp())
+    def log_lik_example_cond_class(self, feature_vector, qclass):
+        #prod=dc.Decimal(1.0)
+        prod=dc.Decimal(0.0)
+        for xi in feature_vector.sparse_vec:
+            if self.feature_probs.has_key((xi, qclass)):
+                prod+=dc.Decimal(self.feature_probs[(xi, qclass)][0]).ln()
+        return prod         
     def lik_example(self, feature_vector):
-        #s=1.0
-        #prod1=1.0
+ 
         s=dc.Decimal(0.0)
         for k in self.classes:
             prod1=dc.Decimal(1.0)
-            for xi in feature_vector.sparse_vec:#range(0, len(feature_vector)):
+            for xi in feature_vector.sparse_vec:
             #    print 'checking prob of feature', xi
                 if self.feature_probs.has_key((xi,k)):
            #         print 'had feature', xi 
@@ -66,25 +81,14 @@ class NaiveBayesClassifier:
     def max_lik_class_given_example(self, feature_vector, class1):
         num=dc.Decimal(1.0000)
         c=self.lik_example(feature_vector)
-        #if c<.00000000000000000001:
-        #    print feature_vector.sparse_vec
-        #    for xi in feature_vector.sparse_vec:
-        #        print 'liklihood of ', xi,  self.feature_probs[(xi, class1)]  
-        for xi in feature_vector.sparse_vec: #range(0,len(feature_vector)):
-            if self.feature_probs.has_key((xi, class1)) and not self.feature_probs[(xi, class1)][0]==0:
-                
+        for xi in feature_vector.sparse_vec:
+            if self.feature_probs.has_key((xi, class1)) :
                 num*=dc.Decimal(self.feature_probs[(xi, class1)][0])
-                #print 'probability of feature' , xi, 'in class', class1, ':' ,self.feature_probs[(xi, class1)]
-                #c=self.lik_example(feature_vector)
-        
-        
         if not c==0:
-            #print 100000.00*num*self.class_likelihood(class1)/(c*100000.00)
-            #print 'in this class we will have', num, '*', self.class_prob[class1], 'over', c, 'without Laplace smoothing'
-            return (num*dc.Decimal(self.class_prob[class1]))/(c)
+            return (num*dc.Decimal(self.class_prob[class1]))/(dc.Decimal(c))
         else:
-        #    print 'sumfin went rong heya' 
             return 0
+
     def get_class_counts(self):
         for k in self.classes:
             count=0
@@ -93,8 +97,9 @@ class NaiveBayesClassifier:
                     count+=1 
              
             self.class_counts.setdefault(k, []).append(count)
+
     def class_likelihood(self,class1):
-        return 1000.00*self.class_counts[class1][0]/(1000.00*len(self.vector_class_pairs))
+        return dc.Decimal(self.class_counts[class1][0])/dc.Decimal(len(self.vector_class_pairs))
        
     def likelihood_feature_given_class(self, feature, qclass):
         count=0
--- SpamFilter.py
+++ SpamFilter.py
@@ -1,6 +1,7 @@
 #SpamFilter
 
 import Learner, TrainingSet, regressionLearner, NaiveBayesClassifier, FreqTextDict, BayesFeatureVector
+import decimal as dc
 import numpy as np
 import ClassifierTrainingSet as cts
 import glob, os
@@ -8,8 +9,8 @@ def main():
     #create a dictionary of words
     dic=FreqTextDict.FreqTextDict()
     dic.populate_from_dirs(['nonspam-train', 'spam-train'])
-    dic.trim_to_size(2000)
-   
+    dic.trim_to_size(2500)
+    print dic.dic
     print len(dic.dic)
     #get training set from data files
     spam=[]
@@ -44,18 +45,32 @@ def main():
     classifier.train()
     print 'getting test data'
     spamcount=0
+    class_as_spam=0
+    nonspamcount=0
+    class_as_nonspam=0
     for f in glob.glob(os.path.join('spam-test', '*.txt')):
-        
+        spamcount+=1
         v1=BayesFeatureVector.BayesFeatureVector()
         #print 'getting features for file', f
         v1.get_features_from_file(dic, f)
         #print 'got that feature vector'
-        print classifier.classify(v1), 'blah spam'
-
+        resp=classifier.classify(v1)
+        if resp[0]==0:
+            class_as_spam+=1 
+        print classifier.classify(v1), 'spam'
+    print 'spam fail rate,',dc.Decimal( class_as_spam)/dc.Decimal(spamcount)
     for f in glob.glob(os.path.join('nonspam-test', '*.txt')):
+        nonspamcount+=1
         v2=BayesFeatureVector.BayesFeatureVector()
         v2.get_features_from_file(dic, f)
-        print classifier.classify(v2), 'blah nonspam'
-   #make a prediction
+        resp2=classifier.classify(v2)
+        print resp2[0]
+        if resp2[0]==1:
+            class_as_nonspam+=1
+        
+        print resp2, 'blah nonspam'
+    print 'nonspam fail rate',dc.Decimal( class_as_nonspam)/dc.Decimal(nonspamcount)
+    print class_as_nonspam, class_as_spam
+  #make a prediction
 if __name__ == "__main__":
     main()
